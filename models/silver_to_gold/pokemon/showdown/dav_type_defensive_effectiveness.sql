6d1e1ed4e2ed05d2d1d0c20b4a0b174cc28e7be5
bbouretdev <bbouretdev@gmail.com>
2025-05-05 10:46:59 +0200

    added type efectiveness stuff

---
+++ models/silver_to_gold/pokemon/showdown/dav_type_defensive_effectiveness.sql
@@ -0,0 +1,49 @@
+WITH typechart AS (
+    SELECT *
+    FROM read_parquet('s3://sandbox-datalake-silver/pokemon/showdown/dav/showdown_dav_typechart.parquet')
+),
+unpivoted AS (
+    SELECT key, 'bug' AS attack_type, dmg_bug AS effectiveness FROM typechart
+    UNION ALL SELECT key, 'dark', dmg_dark FROM typechart
+    UNION ALL SELECT key, 'dragon', dmg_dragon FROM typechart
+    UNION ALL SELECT key, 'electric', dmg_electric FROM typechart
+    UNION ALL SELECT key, 'fairy', dmg_fairy FROM typechart
+    UNION ALL SELECT key, 'fighting', dmg_fighting FROM typechart
+    UNION ALL SELECT key, 'fire', dmg_fire FROM typechart
+    UNION ALL SELECT key, 'flying', dmg_flying FROM typechart
+    UNION ALL SELECT key, 'ghost', dmg_ghost FROM typechart
+    UNION ALL SELECT key, 'grass', dmg_grass FROM typechart
+    UNION ALL SELECT key, 'ground', dmg_ground FROM typechart
+    UNION ALL SELECT key, 'ice', dmg_ice FROM typechart
+    UNION ALL SELECT key, 'normal', dmg_normal FROM typechart
+    UNION ALL SELECT key, 'poison', dmg_poison FROM typechart
+    UNION ALL SELECT key, 'psychic', dmg_psychic FROM typechart
+    UNION ALL SELECT key, 'rock', dmg_rock FROM typechart
+    UNION ALL SELECT key, 'steel', dmg_steel FROM typechart
+    UNION ALL SELECT key, 'stellar', dmg_stellar FROM typechart
+    UNION ALL SELECT key, 'water', dmg_water FROM typechart
+),
+immune AS (
+    SELECT key, attack_type FROM unpivoted WHERE effectiveness = 3
+),
+resistant AS (
+    SELECT key, attack_type FROM unpivoted WHERE effectiveness = 2
+),
+weak AS (
+    SELECT key, attack_type FROM unpivoted WHERE effectiveness = 1
+),
+neutral AS (
+    SELECT key, attack_type FROM unpivoted WHERE effectiveness = 0
+),
+grouped AS (
+    SELECT
+        key AS type_def,
+        (SELECT json_group_array(attack_type) FROM immune WHERE immune.key = u.key) AS immune_to,
+        (SELECT json_group_array(attack_type) FROM resistant WHERE resistant.key = u.key) AS resistant_to,
+        (SELECT json_group_array(attack_type) FROM weak WHERE weak.key = u.key) AS weak_to,
+        (SELECT json_group_array(attack_type) FROM neutral WHERE neutral.key = u.key) AS neutral_to
+    FROM (SELECT DISTINCT key FROM unpivoted) u
+)
+SELECT *
+FROM grouped
+ORDER BY type_def
--- models/silver_to_gold/pokemon/showdown/dav_typechart.sql
+++ models/silver_to_gold/pokemon/showdown/dav_typechart.sql
@@ -1,7 +1,37 @@
 WITH typechart AS (
+    -- Chargement des données depuis le fichier Parquet
     SELECT *
     FROM read_parquet('s3://sandbox-datalake-silver/pokemon/showdown/dav/showdown_dav_typechart.parquet')
+),
+
+-- Transformation des résistances et retrait du préfixe "dmg_"
+typechart_transformed AS (
+    SELECT
+        key AS type,
+        
+        {% set types = [
+            'bug', 'dark', 'dragon', 'electric', 'fairy', 'fighting', 
+            'fire', 'flying', 'ghost', 'grass', 'ground', 'ice', 
+            'normal', 'poison', 'psychic', 'rock', 'steel', 'stellar', 'water'
+        ] %}
+        
+        {% for t in types %}
+            CASE
+                WHEN dmg_{{ t }} = 3 THEN 0
+                WHEN dmg_{{ t }} = 2 THEN 0.5
+                WHEN dmg_{{ t }} = 1 THEN 2
+                WHEN dmg_{{ t }} = 0 THEN 1
+                ELSE dmg_{{ t }}
+            END AS {{ t }},
+        {% endfor %}
+        
+    FROM typechart
 )
 
-SELECT *
-FROM typechart
+-- Sélection des colonnes sans préfixe "dmg_"
+SELECT
+    type,
+    {% for t in types %}
+        {{ t }},
+    {% endfor %}
+FROM typechart_transformed
